3e68ec5b257c35573f73ce8850db7a0e97041dc9
Alex Petrov <alexp@coffeenco.de>
2013-09-09 01:01:41 +0200

    Release 1.0.0-alpha1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gizmo-web/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject gizmo-web/lein-template "1.0.0-apha1"
+  :description "Leiningen template for Gizmo, ClojureWerkz web library"
+  :url "http://github.com/ifesdjeen/gizmo-web-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
