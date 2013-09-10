e6110f74d798dd233b65ed2f67270e583b144975
Alex Petrov <alexp@coffeenco.de>
2013-09-10 19:13:02 +0200

    Add an ability to create a project with bootstrap already

--- src/leiningen/new/gizmo_web.clj
+++ src/leiningen/new/gizmo_web.clj
@@ -1,13 +1,15 @@
 (ns leiningen.new.gizmo-web
-  (:use [leiningen.new.templates :only [renderer name-to-path ->files]]))
+  (:require [clojure.java.io :as io]
+            [leiningen.new.templates :refer [renderer name-to-path ->files *dir*]]))
 
 (def render (renderer "gizmo-web"))
 
 (defn gizmo-web
   "FIXME: write documentation"
-  [name]
-  (let [data {:name name
-              :sanitized (name-to-path name)}]
+  [name & more]
+  (let [options (set more)
+        data    {:name name
+                 :sanitized (name-to-path name)}]
     (->files data
              ["project.clj" (render "project.clj" data)]
              ["config/development.clj" (render "config/development.clj" data)]
@@ -34,4 +36,36 @@
              ["resources/templates/shared/header.html" (render "resources/templates/shared/header.html" data)]
 
              ["resources/public/stylesheets/application.css" (render "resources/public/stylesheets/application.css" data)]
-             ["resources/public/javascripts/application.js" (render "resources/public/javascripts/application.js" data)])))
+             ["resources/public/javascripts/application.js" (render "resources/public/javascripts/application.js" data)])
+
+    (when (get options "--bootstrap")
+      (binding [*dir* (-> (System/getProperty "leiningen.original.pwd")
+                          (io/file name) (.getPath))]
+        (->files data
+                 ["resources/templates/layouts/application.html" (render "resources_bootstrap/templates/layouts/application.html" data)]
+                 ["resources/templates/home/index.html" (render "resources_bootstrap/templates/home/index.html" data)]
+                 ["resources/templates/shared/header.html" (render "resources_bootstrap/templates/shared/header.html" data)]
+
+                 ["resources/public/stylesheets/application.css" (render "resources/public/stylesheets/application.css" data)]
+                 ["resources/public/javascripts/application.js" (render "resources/public/javascripts/application.js" data)]
+
+                 ["resources/public/fonts/glyphicons-halflings-regular.eot"
+                  (render "resources_bootstrap/public/fonts/glyphicons-halflings-regular.eot" data)]
+                 ["resources/public/fonts/glyphicons-halflings-regular.svg"
+                  (render "resources_bootstrap/public/fonts/glyphicons-halflings-regular.svg" data)]
+                 ["resources/public/fonts/glyphicons-halflings-regular.ttf"
+                  (render "resources_bootstrap/public/fonts/glyphicons-halflings-regular.ttf" data)]
+                 ["resources/public/fonts/glyphicons-halflings-regular.woff"
+                  (render "resources_bootstrap/public/fonts/glyphicons-halflings-regular.woff" data)]
+
+                 ["resources/public/javascripts/vendor/bootstrap.min.js"
+                  (render "resources_bootstrap/public/javascripts/vendor/bootstrap.min.js" data)]
+                 ["resources/public/javascripts/vendor/jquery-2.0.3.min.js"
+                  (render "resources_bootstrap/public/javascripts/vendor/jquery-2.0.3.min.js" data)]
+
+                 ["resources/public/stylesheets/vendor/bootstrap-theme.min.css"
+                  (render "resources_bootstrap/public/stylesheets/vendor/bootstrap-theme.min.css" data)]
+                 ["resources/public/stylesheets/vendor/bootstrap.min.css"
+                  (render "resources_bootstrap/public/stylesheets/vendor/bootstrap.min.css" data)]
+
+                 )))))
--- src/leiningen/new/gizmo_web/project.clj
+++ src/leiningen/new/gizmo_web/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clojurewerkz/gizmo "1.0.0-alpha1"]
+                 [clojurewerkz/gizmo "1.0.0-alpha2-SNAPSHOT"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/tools.cli "0.2.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]}}
